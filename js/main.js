7bd6c4eb7f54a169b54e5f57b204618b15746c75
AnhTran <anhtran0194@gmail.com>
2025-11-12 13:55:07 +1100

    fix loading

--- js/main.js
+++ js/main.js
@@ -82,41 +82,41 @@
 
 
 
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
+	// var contentWayPoint = function() {
+	// 	var i = 0;
+	// 	$('.animate-box').waypoint( function( direction ) {
 
-			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
+	// 		if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
 				
-				i++;
+	// 			i++;
 
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
+	// 			$(this.element).addClass('item-animate');
+	// 			setTimeout(function(){
 
-					$('body .animate-box.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn animated-fast');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft animated-fast');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight animated-fast');
-							} else {
-								el.addClass('fadeInUp animated-fast');
-							}
+	// 				$('body .animate-box.item-animate').each(function(k){
+	// 					var el = $(this);
+	// 					setTimeout( function () {
+	// 						var effect = el.data('animate-effect');
+	// 						if ( effect === 'fadeIn') {
+	// 							el.addClass('fadeIn animated-fast');
+	// 						} else if ( effect === 'fadeInLeft') {
+	// 							el.addClass('fadeInLeft animated-fast');
+	// 						} else if ( effect === 'fadeInRight') {
+	// 							el.addClass('fadeInRight animated-fast');
+	// 						} else {
+	// 							el.addClass('fadeInUp animated-fast');
+	// 						}
 
-							el.removeClass('item-animate');
-						},  k * 200, 'easeInOutExpo' );
-					});
+	// 						el.removeClass('item-animate');
+	// 					},  k * 200, 'easeInOutExpo' );
+	// 				});
 					
-				}, 100);
+	// 			}, 100);
 				
-			}
+	// 		}
 
-		} , { offset: '65%' } );
-	};
+	// 	} , { offset: '65%' } );
+	// };
 
 
 	var dropdown = function() {
